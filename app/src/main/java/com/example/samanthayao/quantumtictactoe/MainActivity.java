f4e59c759a98dee2454afa40207bcf9dc8f8a3bd
Samantha Yao <yaosa@utschools.ca>
2017-07-30 17:46:54 -0400

    made a board

--- app/src/main/java/com/example/samanthayao/quantumtictactoe/MainActivity.java
+++ app/src/main/java/com/example/samanthayao/quantumtictactoe/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.samanthayao.quantumtictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import static android.R.id.list;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView board;
+
+        board = (GridView) findViewById(R.id.gridview);
+        board.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item));
+
     }
 }
